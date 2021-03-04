99a1092bdb7fa06c6e84d0ef8422e4c5bdd27064
Kiah Pleasant <kiahpleasant@gmail.com>
2021-03-04 11:09:32 -0500

    push to gh

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,30 @@
+
+const User = require('../models/User');
+
+
 module.exports.signup_get = (req, res) =>{
     res.render('signup');
 }
 module.exports.login_get = (req, res) =>{
     res.render('login');
 }
-module.exports.signup_post = (req, res) =>{
+module.exports.signup_post = async (req, res) =>{
+    const {email, password} = req.body;
+    try{
+      User.create({email, password});
+    }
+    catch(err){
+
+    }
+
+    console.log(email, password)
     res.send('new signup');
 }
-module.exports.login_post = (req, res) =>{
+
+
+module.exports.login_post = async (req, res) =>{
+    const {email, password} = req.body;
+
+    console.log(email, password)
     res.render('user login');
 }
